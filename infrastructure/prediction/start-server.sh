75404ff2bca09d8f6c8e5df4f70d3a649d4c2728
George-Andrei Iosif <georgeiosif@live.com>
2020-12-23 18:38:53 +0200

    Adds Docker and Docker Compose files

---
+++ infrastructure/prediction/start-server.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Wait the volume to be mounted and install the requirements
+until cd /dike/dike && pip install -r requirements.txt
+do
+    echo "Retrying pip install.."
+done
+
+# Run the program
+cd /dike/dike/prediction
+python app.py
